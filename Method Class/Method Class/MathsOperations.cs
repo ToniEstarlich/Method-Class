12223a3bb70921e65bc4b42eb975577b3355e75e
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2023-12-07 21:47:56 +0000

    Add files via upload

---
+++ Method Class/Method Class/MathsOperations.cs
@@ -0,0 +1,17 @@
+﻿// MathOperations.cs
+using System;
+
+public class MathOperations
+{
+    // This method takes two integers as parameters,
+    // performs a math operation on the first integer,
+    // and displays the second integer to the screen.
+    public void PerformMathOperation(int firstNumber, int secondNumber)
+    {
+        // Perform a simple math operation (for example, multiplication) on the first number.
+        int result = firstNumber * 2;
+
+        // Display the second number to the console.
+        Console.WriteLine($"Result: {result}, Second Number: {secondNumber}");
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program.cs
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Step 2: Instantiate the MathOperations class.
+        MathOperations mathObj = new MathOperations();
+
+        // Step 3: Call the method in the class, passing in two numbers.
+        mathObj.PerformMathOperation(3, 7);
+
+        // Step 4: Call the method in the class, specifying the parameters by name.
+        mathObj.PerformMathOperation(firstNumber: 5, secondNumber: 10);
+    }
+}
